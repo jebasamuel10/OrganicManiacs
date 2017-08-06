3cec4269982ab72bf3b6598553ccedc6b3a41e42
GoldenProductWS <akshiraj.lnt@gmail.com>
2017-08-06 22:50:46 +0530

    Update ProductServiceImpl.java

--- src/main/java/com/organically4u/serviceimpl/ProductServiceImpl.java
+++ src/main/java/com/organically4u/serviceimpl/ProductServiceImpl.java
@@ -1,108 +1,241 @@
-package com.organically4u.serviceimpl;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.stereotype.Service;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.WriteResult;
-import com.mongodb.gridfs.GridFSDBFile;
-import com.organically4u.model.Product;
-import com.organically4u.model.dto.ProductDTO;
-import com.organically4u.service.ProductService;
-import com.organically4u.util.Converter;
-
-@Service
-public class ProductServiceImpl implements ProductService{
+	package com.organically4u.serviceimpl;
 	
-	private static Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
-	@Autowired
-	private MongoTemplate mongoTemplate;
-	@Autowired
-	private GridFsTemplate gridFsTemplate;
-	@Autowired
-	private Converter converter;
+	import java.io.FileInputStream;
+	import java.io.FileNotFoundException;
+	import java.io.InputStream;
+	import java.util.ArrayList;
+	import java.util.List;
 	
+	import org.slf4j.Logger;
+	import org.slf4j.LoggerFactory;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.data.mongodb.core.MongoTemplate;
+	import org.springframework.data.mongodb.core.query.Criteria;
+	import org.springframework.data.mongodb.core.query.Query;
+	import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+	import org.springframework.stereotype.Service;
 	
-	public GridFsTemplate getGridFsTemplate() {
-		return gridFsTemplate;
-	}
-
-
-	public void setGridFsTemplate(GridFsTemplate gridFsTemplate) {
-		this.gridFsTemplate = gridFsTemplate;
-	}
-
-
-	public MongoTemplate getMongoTemplate() {
-		return mongoTemplate;
-	}
-
-
-	public void setMongoTemplate(MongoTemplate mongoTemplate) {
-		this.mongoTemplate = mongoTemplate;
-	}
-
-	@Override
-	public List<ProductDTO> getAllProducts(){
+	import com.mongodb.BasicDBObject;
+	import com.mongodb.WriteResult;
+	import com.mongodb.gridfs.GridFSDBFile;
+	import com.organically4u.model.Product;
+	import com.organically4u.model.dto.ProductDTO;
+	import com.organically4u.service.ProductService;
+	import com.organically4u.util.Converter;
+	
+	import groovy.util.logging.Slf4j;
+	
+	@Slf4j
+	@Service
+	public class ProductServiceImpl implements ProductService{
 		
-		List<Product> productlist = new ArrayList<Product>();
-		List<ProductDTO> productDTOlist = new ArrayList<ProductDTO>();
-		productlist.addAll(mongoTemplate.findAll(Product.class));
-		for(Product product :productlist){
-			productDTOlist.add((ProductDTO)converter.convert(ProductDTO.class, product ));
+		private static Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
+		
+		@Autowired
+		private MongoTemplate mongoTemplate;
+		@Autowired
+		private GridFsTemplate gridFsTemplate;
+		@Autowired
+		private Converter converter;
+		
+		
+		
+		
+		// Posting
+		
+		
+			@Override
+			public String addProduct(ProductDTO productDTO) {
+				// TODO Auto-generated method stub
+				try {
+					Product product = (Product)converter.convert(Product.class, productDTO );
+					mongoTemplate.insert(product);
+					return product.getId_product();
+				}catch(Exception e) {
+					e.printStackTrace();
+					logger.error(e.toString());
+					return null;
+				}
+			}
+	
+	
+			@Override
+			public String updateProductDetails(ProductDTO productDTO) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			
+			@Override
+			public String deleteProduct(String id) {
+				// TODO Auto-generated method stub
+				WriteResult writeResult = mongoTemplate.remove(getProduct(id));
+				return writeResult.wasAcknowledged() ? id : null;
+			}
+			
+			
+			@Override
+			public String addCategory(ProductDTO productDTO) {
+				try {
+					Product product = (Product)converter.convert(Product.class, productDTO );
+					mongoTemplate.insert(product);
+					return product.getId_product();
+				}catch(Exception e) {
+					e.printStackTrace();
+					logger.error(e.toString());
+					return null;
+				}
+			}
+			
+			@Override
+			public String updateCategory(String id) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+	
+	
+			@Override
+			public String deleteCategory(String id) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			
+			@Override
+			public String addSourceFrom(ProductDTO productDTO) {
+				try {
+					Product product = (Product)converter.convert(Product.class, productDTO );
+					mongoTemplate.insert(product);
+					return product.getId_product();
+				}catch(Exception e) {
+					e.printStackTrace();
+					logger.error(e.toString());
+					return null;
+				}
+			}
+	
+			@Override
+			public String updateSourceFrom(String id) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String delteSourceFrom(String id) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String addCustomer(ProductDTO productDTO) {
+				try {
+					Product product = (Product)converter.convert(Product.class, productDTO );
+					mongoTemplate.insert(product);
+					return product.getId_product();
+				}catch(Exception e) {
+					e.printStackTrace();
+					logger.error(e.toString());
+					return null;
+				}
+			}
+	
+			@Override
+			public String updateCustomer(String id) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String delteCustomer(String id) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		// Posting -End	
+			
+		
+		
+		//Enquiry - Start
+		
+		@Override
+		public List<ProductDTO> getAllProducts(){
+			
+			List<Product> productlist = new ArrayList<Product>();
+			List<ProductDTO> productDTOlist = new ArrayList<ProductDTO>();
+			productlist.addAll(mongoTemplate.findAll(Product.class));
+			for(Product product :productlist){
+				productDTOlist.add((ProductDTO)converter.convert(ProductDTO.class, product ));
+			}
+			return productDTOlist;
 		}
-		return productDTOlist;
-	}
-
-	@Override
-	public String addProduct(ProductDTO productDTO) {
-		// TODO Auto-generated method stub
-		try {
-			Product product = (Product)converter.convert(Product.class, productDTO );
-			mongoTemplate.insert(product);
-			return product.getId();
-		}catch(Exception e) {
-			e.printStackTrace();
-			logger.error(e.toString());
+	
+		@Override
+		public List<ProductDTO> getAllCategories() {
+			// TODO Auto-generated method stub
 			return null;
 		}
+	
+	
+		@Override
+		public List<ProductDTO> getAllSrcfroms() {
+			// TODO Auto-generated method stub
+			return null;
 		}
-
-
-	@Override
-	public String deleteProduct(String id) {
-		// TODO Auto-generated method stub
-		WriteResult writeResult = mongoTemplate.remove(getProduct(id));
-		return writeResult.wasAcknowledged() ? id : null;
-	}
 	
-	public Product getProduct(String id){
-		Query query = new Query();
-		query.addCriteria(Criteria.where("_id").is(id));
-		return mongoTemplate.findOne(query,Product.class);
-	}
 	
-	public String storeProductImage() throws FileNotFoundException {
-		InputStream inputStream = new FileInputStream("src/main/resources/images/test.jpg"); 
-		String id = gridFsTemplate.store(inputStream, "test.jpg", "image/jpg", new BasicDBObject("id","image1")).getId().toString();
-		return id;
-	}
+		@Override
+		public ProductDTO getSrcfrom() {
+			// TODO Auto-generated method stub
+			return null;
+		}
 	
-	public GridFSDBFile retrieveProductImage() throws FileNotFoundException {
-		String id = "5967b1094aabce03ac818d68";
-		GridFSDBFile gridFsdbFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(id)));
-		return gridFsdbFile;
+	
+		@Override
+		public ProductDTO getCategories() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public Product getProduct(String id){
+			Query query = new Query();
+			query.addCriteria(Criteria.where("_id").is(id));
+			return mongoTemplate.findOne(query,Product.class);
+		}
+		
+		public String storeProductImage() throws FileNotFoundException {
+			InputStream inputStream = new FileInputStream("src/main/resources/images/test.jpg"); 
+			String id = gridFsTemplate.store(inputStream, "test.jpg", "image/jpg", new BasicDBObject("id","image1")).getId().toString();
+			return id;
+		}
+		
+		public GridFSDBFile retrieveProductImage() throws FileNotFoundException {
+			String id = "5967b1094aabce03ac818d68";
+			GridFSDBFile gridFsdbFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(id)));
+			return gridFsdbFile;
+		}
+		
+	
+	
+	
+		
+		public GridFsTemplate getGridFsTemplate() {
+			return gridFsTemplate;
+		}
+	
+	
+		public void setGridFsTemplate(GridFsTemplate gridFsTemplate) {
+			this.gridFsTemplate = gridFsTemplate;
+		}
+	
+	
+		public MongoTemplate getMongoTemplate() {
+			return mongoTemplate;
+		}
+	
+	
+		public void setMongoTemplate(MongoTemplate mongoTemplate) {
+			this.mongoTemplate = mongoTemplate;
+		}
+		
+	
 	}
-}

47099db529278c31e1e499a79f06d7d977b4bfb8
GoldenProductWS <akshiraj.lnt@gmail.com>
2017-08-12 15:42:11 +0530

    CategoryDTO

--- src/main/java/com/organically4u/service/ProductService.java
+++ src/main/java/com/organically4u/service/ProductService.java
@@ -1,11 +1,11 @@
 package com.organically4u.service;
 
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.List;
 
 import com.mongodb.gridfs.GridFSDBFile;
 import com.organically4u.model.Product;
+import com.organically4u.model.dto.CategoryDTO;
 import com.organically4u.model.dto.ProductDTO;
 
 public interface ProductService {
@@ -15,7 +15,7 @@ public interface ProductService {
 	public String updateProductDetails(ProductDTO productDTO);
 	public String deleteProduct(String id);
 	
-	public String addCategory(ProductDTO productDTO);
+	public String addCategory(CategoryDTO categoryDTO);
 	public String deleteCategory(String id);
 	public String updateCategory(String id);
 	
@@ -44,3 +44,4 @@ public interface ProductService {
 	public GridFSDBFile retrieveProductImage() throws FileNotFoundException;
 	
 }
+
